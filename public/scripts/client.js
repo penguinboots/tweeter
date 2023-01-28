5cc8a284b0c38053c4a69103d66618a282309cbf
penguinboots <sabrina.ynw@gmail.com>
2023-01-28 12:53:25 -0800

    add event listener for form submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const sampleTweet = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
   // create tweet article element given raw tweet data
   const createTweetElement = function(tweet) {
@@ -31,7 +45,7 @@ $(document).ready(() => {
     </header>
     <div class="tweet-body">${tweet.content.text}</div>
     <footer>
-      <div><output name="days-ago">${tweet.create_at}</output> ago</div>
+      <div><output name="days-ago">${tweet.created_at}</output> ago</div>
       <div>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -47,14 +61,22 @@ $(document).ready(() => {
   const renderTweets = function(tweets) {
     const $container = $(".tweets-container");
     $container.empty();
-    for (const tweet in tweets) {
+    for (const tweet of tweets) {
       const oneTweet = createTweetElement(tweet);
       $container.append(oneTweet);
     }
   };
 
-  let $tweet = createTweetElement(sampleTweet);
-  $(".tweets-container").append($tweet);
+  renderTweets(data);
 
-  console.log($tweet);
+  // submit event listener
+  $(".new-tweet form").submit(function(event) {
+    event.preventDefault();
+    console.log($(this).find("textarea").val());
+    const serializedData = $(this).serialize();
+    console.log(serializedData);
+    $.post("/tweets", serializedData)
+      .then(renderTweets(data));
+  });
+
 });
