213b4032d2c821a779d2b5d58eac497cbc80eee3
penguinboots <sabrina.ynw@gmail.com>
2023-01-28 12:59:21 -0800

    add loadTweets, remove hardcoded tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // create tweet article element given raw tweet data
   const createTweetElement = function(tweet) {
     const tweetArticle = $(`
@@ -67,7 +41,13 @@ $(document).ready(() => {
     }
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.get("/tweets", function(data, status) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 
   // submit event listener
   $(".new-tweet form").submit(function(event) {
